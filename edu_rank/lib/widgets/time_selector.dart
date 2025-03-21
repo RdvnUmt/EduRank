c99c331e544b28b47750a67902badf48877c6afc
Rıdvan Umut <r.umutunal@gmail.com>
2025-03-21 22:09:54 +0300

    zamanlayıcı tasarımı yenilendi

--- edu_rank/lib/widgets/time_selector.dart
+++ edu_rank/lib/widgets/time_selector.dart
@@ -23,13 +23,13 @@ class TimeSelector extends StatelessWidget {
         children: [
           Text(
             label,
-            style: const TextStyle(fontWeight: FontWeight.bold),
+            style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 16),
           ),
           const SizedBox(height: 8),
           Container(
             height: 120,
             decoration: BoxDecoration(
-              color: Colors.grey.shade200,
+              color: const Color(0xFFF5F5F5),
               borderRadius: BorderRadius.circular(12),
             ),
             child: NumberPicker(
@@ -44,7 +44,7 @@ class TimeSelector extends StatelessWidget {
               selectedTextStyle: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
-                color: Theme.of(context).primaryColor,
+                color: const Color.fromARGB(255, 0, 0, 0),
               ),
               decoration: BoxDecoration(
                 border: Border(
